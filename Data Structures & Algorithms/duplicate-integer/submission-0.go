14b37b18ef2fd81827414bb1d5cd5422acfdce87
arvindpathare1993-sys <arvindpathare1993@gmail.com>
2026-09-23 06:27:40 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[j] == nums[i] {
+                return true
+            }
+        }
+
+    
+    }
+    return false
+}
+
